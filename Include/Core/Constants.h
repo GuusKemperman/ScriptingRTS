b294d2135b0e0f0591156b6f68076d54946c6f0e
GuusKemperman <guus.kemperman@gmail.com>
2025-03-09 14:03:20 +0100

    Changed simulation constants

--- Include/Core/Constants.h
+++ Include/Core/Constants.h
@@ -2,9 +2,9 @@
 
 namespace RTS::Constants
 {
-	static constexpr uint32 sNumSimulationStepsBetweenEvaluate = 10;
+	static constexpr uint32 sNumSimulationStepsBetweenEvaluate = 1;
 	static constexpr float sSimulationStepSize = .1f;
-	static constexpr float sSimulationDurationSeconds = 60.0f;
+	static constexpr float sSimulationDurationSeconds = 240.0f;
 
 	static constexpr float sEvaluateStepSize = sNumSimulationStepsBetweenEvaluate * sSimulationStepSize;
 	static constexpr uint32 sTotalNumEvaluateSteps = static_cast<uint32>(sSimulationDurationSeconds / sEvaluateStepSize);
