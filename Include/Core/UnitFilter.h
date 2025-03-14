757d212ca8ee548822f26403cfb24412f8abc2a7
GuusKemperman <guus.kemperman@gmail.com>
2025-03-14 15:20:58 +0100

    Added health filter

--- Include/Core/UnitFilter.h
+++ Include/Core/UnitFilter.h
@@ -10,17 +10,31 @@ namespace RTS
 {
 	enum class TeamFilter : uint8 { Any, Friendly, Enemy, Myself };
 	enum class SortPriority : uint8 { Nearest, Farthest };
+
 	enum class RangeFilter : uint8 {
 		Any,
 		InShortRange,
-		WithinShortRange,
+		InShortRangeOrCloser,
 		OutShortRange,
 		InMidRange,
-		WithinMidRange,
+		InMidRangeOrCloser,
 		OutMidRange,
 		InLongRange,
-		WithinLongRange,
+		InLongRangeOrCloser,
 		OutLongRange,
+	};
+
+	enum class HealthFilter : uint8
+	{
+		Any,
+		Below25,
+		Above25,
+		Below50,
+		Above50,
+		Below75,
+		Above75,
+		Below100,
+		Full,
 	};
 
 	struct UnitFilter
@@ -36,6 +50,7 @@ namespace RTS
 		TeamFilter mTeam{};
 		SortPriority mSortByDistance{};
 		RangeFilter mRange{};
+		HealthFilter mHealth{};
 
 	private:
 		friend CE::ReflectAccess;
@@ -62,6 +77,12 @@ struct Reflector<RTS::RangeFilter>
 	static CE::MetaType Reflect();
 };
 
+template<>
+struct Reflector<RTS::HealthFilter>
+{
+	static CE::MetaType Reflect();
+};
+
 #ifdef EDITOR
 IMGUI_AUTO_DEFINE_INLINE(template<>, RTS::UnitFilter, var.DisplayWidget(name);)
 #endif // EDITOR
@@ -78,8 +99,13 @@ namespace cereal
 		{
 			ar(value.mRange);
 		}
+
+		if (version >= 2)
+		{
+			ar(value.mHealth);
+		}
 	}
 }
 
-CEREAL_CLASS_VERSION(RTS::UnitFilter, 1);
+CEREAL_CLASS_VERSION(RTS::UnitFilter, 2);
 
--- Source/Core/UnitFilter.cpp
+++ Source/Core/UnitFilter.cpp
@@ -4,10 +4,13 @@
 #include <entt/entity/runtime_view.hpp>
 #include <magic_enum/magic_enum_format.hpp>
 
+#include "Components/HealthComponent.h"
 #include "Components/TeamTag.h"
 #include "Components/WeaponComponent.h"
 #include "Components/Physics2D/DiskColliderComponent.h"
+#include "Core/GameState.h"
 #include "Core/RTSCollisionLayers.h"
+#include "Core/UnitTypes.h"
 #include "Meta/MetaProps.h"
 #include "Meta/MetaReflect.h"
 #include "Meta/MetaType.h"
@@ -22,6 +25,7 @@ void RTS::UnitFilter::DisplayWidget(const std::string& )
 	CE::ShowInspectUI("Team", mTeam);
 	CE::ShowInspectUI("SortByDistance", mSortByDistance);
 	CE::ShowInspectUI("Range", mRange);
+	CE::ShowInspectUI("Health", mHealth);
 }
 #endif
 
@@ -125,22 +129,77 @@ entt::entity RTS::UnitFilter::operator()(const CE::World& world, entt::entity re
 			switch (mRange)
 			{
 			case RangeFilter::InShortRange:		return { -std::numeric_limits<float>::infinity(), range.mShortRangeEnd };
-			case RangeFilter::WithinShortRange:	return { -std::numeric_limits<float>::infinity(), range.mShortRangeEnd };
+			case RangeFilter::InShortRangeOrCloser:	return { -std::numeric_limits<float>::infinity(), range.mShortRangeEnd };
 			case RangeFilter::OutShortRange:	return { range.mShortRangeEnd, std::numeric_limits<float>::infinity() };
 			case RangeFilter::InMidRange:		return { range.mShortRangeEnd, range.mMidRangeEnd };
-			case RangeFilter::WithinMidRange:	return { -std::numeric_limits<float>::infinity(), range.mMidRangeEnd };
+			case RangeFilter::InMidRangeOrCloser:	return { -std::numeric_limits<float>::infinity(), range.mMidRangeEnd };
 			case RangeFilter::OutMidRange:		return { range.mMidRangeEnd, std::numeric_limits<float>::infinity() };
 			case RangeFilter::InLongRange:		return { range.mMidRangeEnd, range.mLongRangeEnd }; 
-			case RangeFilter::WithinLongRange:	return { -std::numeric_limits<float>::infinity(), range.mLongRangeEnd };
+			case RangeFilter::InLongRangeOrCloser:	return { -std::numeric_limits<float>::infinity(), range.mLongRangeEnd };
 			case RangeFilter::OutLongRange:		return { range.mLongRangeEnd, std::numeric_limits<float>::infinity() };
 			}
 			ABORT;
 			return {};
 		}();
 
+	struct HealthCheck
+	{
+		HealthCheck(HealthFilter mHealth) :
+			mHealthFilter(mHealth)
+		{
+			if (mHealth == HealthFilter::Any)
+			{
+				return;
+			}
+
+			mHealthStorage = reg.Storage<HealthComponent>();
+			mUnitTypeStorage = reg.Storage<UnitType::Enum>();
+			ASSERT(mHealthStorage != nullptr && mUnitTypeStorage != nullptr);
+		}
+
+		bool operator()(entt::entity entity) const
+		{
+			if (mHealthFilter == HealthFilter::Any)
+			{
+				return true;
+			}
+
+			if (!mUnitTypeStorage->contains(entity)
+				|| !mHealthStorage->contains(entity))
+			{
+				return false;
+			}
+
+			const UnitType unitType = GetUnitType(mUnitTypeStorage->get(entity));
+			const float unitHealth = mHealthStorage->get(entity).mHealth;
+			const float healthPercentage = unitHealth / unitType.mHealth;
+
+			switch (mHealthFilter)
+			{
+			case HealthFilter::Below25: return healthPercentage <  0.25f;
+			case HealthFilter::Above25: return healthPercentage >= 0.25f;
+			case HealthFilter::Below50: return healthPercentage <  0.50f;
+			case HealthFilter::Above50: return healthPercentage >= 0.50f;
+			case HealthFilter::Below75: return healthPercentage <  0.75f;
+			case HealthFilter::Above75: return healthPercentage >= 0.75f;
+			case HealthFilter::Below100:return healthPercentage <  1.00f;
+			case HealthFilter::Full:	return healthPercentage >= 1.00f;
+			}
+
+			ABORT;
+			return false;
+		}
+
+		HealthFilter mHealthFilter{};
+		const entt::storage_for_t<HealthComponent>* mHealthStorage{};
+		const entt::storage_for_t<UnitType::Enum>* mUnitTypeStorage{};
+	} healthCheck{ mHealth };
+
 	auto isValidEntity = [&](entt::entity entity)
 		{
-			return entity != requestedByUnit && view.contains(entity);
+			return entity != requestedByUnit
+					&& view.contains(entity)
+					&& healthCheck(entity);
 		};
 
 	auto selectEntity = [&](entt::entity entity, float dist)
@@ -206,4 +265,9 @@ CE::MetaType Reflector<RTS::SortPriority>::Reflect()
 CE::MetaType Reflector<RTS::RangeFilter>::Reflect()
 {
 	return CE::ReflectEnumType<RTS::RangeFilter>(true);
+}
+
+CE::MetaType Reflector<RTS::HealthFilter>::Reflect()
+{
+	return CE::ReflectEnumType<RTS::HealthFilter>(true);
 }
